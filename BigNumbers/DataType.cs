daa2be80c54c97795db21864b2fabc972f70d3f7
Anastasiia Liahera <Liahera@outlook.com>
2023-05-09 22:34:48 +0300

    Add project files.

---
+++ BigNumbers/DataType.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigNumbers
+{
+    public class DataType
+    {
+        ulong array;
+
+        public string Array
+        {
+            get;
+            set;
+        }
+
+        public string INV(string value)
+        {
+            string valueINV = "";
+            for (int i = 0; i < value.Length; i++)
+            {
+                valueINV += (~UInt64.Parse(value[i].ToString(), NumberStyles.AllowHexSpecifier)).ToString();
+            }
+            return valueINV;
+        }
+
+        public string XOR(string value, string value2)
+        {
+            string valueXOR = "";
+            for (int i = 0; i < value.Length; i++)
+            {
+                valueXOR += (UInt64.Parse(value[i].ToString(), NumberStyles.AllowHexSpecifier) ^ UInt64.Parse(value2[i].ToString(), NumberStyles.AllowHexSpecifier)).ToString();
+            }
+            return valueXOR;
+        }
+
+        public string AND(string value, string value2)
+        {
+            string valueXOR = "";
+            for (int i = 0; i < value.Length; i++)
+            {
+                valueXOR += (UInt64.Parse(value[i].ToString(), NumberStyles.AllowHexSpecifier) & UInt64.Parse(value2[i].ToString(), NumberStyles.AllowHexSpecifier)).ToString();
+            }
+            return valueXOR;
+        }
+
+        public string ShiftR(string value, int n)
+        {
+            string valueINV = "";
+            for (int i = 0; i < value.Length; i++)
+            {
+                valueINV += (UInt64.Parse(value[i].ToString(), NumberStyles.AllowHexSpecifier) >> n).ToString();
+            }
+            return valueINV;
+        }
+
+        public string ShiftL(string value, int n)
+        {
+            string valueINV = "";
+            for (int i = 0; i < value.Length; i++)
+            {
+                valueINV += (UInt64.Parse(value[i].ToString(), NumberStyles.AllowHexSpecifier) << n).ToString();
+            }
+            return valueINV;
+        }
+        public string ADD(string value, string value2)
+        {
+            string valueXOR = "";
+            for (int i = 0; i < value.Length; i++)
+            {
+                valueXOR += (UInt64.Parse(value[i].ToString(), NumberStyles.AllowHexSpecifier) + UInt64.Parse(value2[i].ToString(), NumberStyles.AllowHexSpecifier)).ToString();
+            }
+            return valueXOR;
+        }
+    };
+}
---
+++ BigNumbers/Program.cs
@@ -0,0 +1,9 @@
+﻿using BigNumbers;
+
+DataType numberC = new DataType();
+Console.WriteLine(numberC.INV("51bf608414ad5726a3c1bec098f77b1b54ffb2787f8d528a74c1d7fde6470ea4"));
+Console.WriteLine(numberC.XOR("51bf608414ad5726a3c1bec098f77b1b54ffb2787f8d528a74c1d7fde6470ea4", "403db8ad88a3932a0b7e8189aed9eeffb8121dfac05c3512fdb396dd73f6331c"));
+Console.WriteLine(numberC.AND("51bf608414ad5726a3c1bec098f77b1b54ffb2787f8d528a74c1d7fde6470ea4", "403db8ad88a3932a0b7e8189aed9eeffb8121dfac05c3512fdb396dd73f6331c"));
+Console.WriteLine(numberC.ShiftR("51bf608414ad5726a3c1bec098f77b1b54ffb2787f8d528a74c1d7fde6470ea4", 3));
+Console.WriteLine(numberC.ShiftL("51bf608414ad5726a3c1bec098f77b1b54ffb2787f8d528a74c1d7fde6470ea4", 3));
+Console.WriteLine(numberC.ADD("36f028580bb02cc8272a9a020f4200e346e276ae664e45ee80745574e2f5ab80", "70983d692f648185febe6d6fa607630ae68649f7e6fc45b94680096c06e4fadb"));
